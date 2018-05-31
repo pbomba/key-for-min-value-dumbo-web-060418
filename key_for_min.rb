79aab5098a86de04b6fd5b378dd5c3a866c1b51a
Paul Bomba <paul@paulbomba.com>
2018-05-31 15:00:29 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-keyArray = []
-valueArray = []
-  name_hash.each do |k, v|
-    keyArray << k
-    valueArray << v
+  keyArray = []
+  valueArray = []
+    name_hash.each do |k, v|
+      keyArray << k
+      valueArray << v
+    end
+  while i < valueArray.length
+  
+  i += 1
   end
-  puts keyArray
-  puts valueArray
 end
